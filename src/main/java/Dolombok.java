29e24e3f1792c0d3e7de78572d678bdf271d495e
wuwenbo <1300910364@qq.com>
2020-11-06 19:54:28 +0800

    初始化

--- src/main/java/Dolombok.java
+++ src/main/java/Dolombok.java
@@ -1,12 +1,15 @@
 import generate.TCustomer;
 import generate.TCustomerDao;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Dolombok {
     @Mapper
     static
     TCustomerDao tCustomerDao;
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         TCustomer record = new TCustomer();
         tCustomerDao.insert(record);
     }
